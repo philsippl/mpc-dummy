c23d0857976a5ff6b0a3adfbdd44ff97dd0eac0c
Philipp Sippl <philsippl@gmail.com>
2025-10-26 16:41:09 +0100

    up

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use iris_mpc_cpu::{
     },
     shares::RingElement,
 };
+use futures::{stream::FuturesUnordered, StreamExt};
 use itertools::Itertools;
 use rand::{CryptoRng, Rng, SeedableRng, rngs::StdRng};
 use std::{cmp::{max, min}, sync::Arc, time::Instant};
@@ -299,7 +300,8 @@ impl Actor {
 
         let db = Arc::clone(&self.db);
         let db_len = db.len();
-        let chunk_size = max(1, (db_len + num_network_workers - 1) / num_network_workers);
+        let target_chunks = max(1, num_network_workers * 4);
+        let chunk_size = max(1, (db_len + target_chunks - 1) / target_chunks);
 
         let mut chunk_bounds = Vec::new();
         let mut offset = 0usize;
@@ -310,32 +312,39 @@ impl Actor {
         }
 
         let chunk_count = chunk_bounds.len();
-        let (result_tx, mut result_rx) =
-            mpsc::channel::<(usize, Vec<bool>, u64, u64, u64, u64)>(max(1, chunk_count));
-        let mut worker_handles = Vec::new();
-
+        let mut cpu_tasks = FuturesUnordered::new();
         for (chunk_id, (chunk_start, chunk_end)) in chunk_bounds.into_iter().enumerate() {
-            let mut session = self.network.sessions.get().await?;
-            let result_tx = result_tx.clone();
-            let query = Arc::clone(&shared_vector);
             let db = Arc::clone(&db);
-
-            let handle = task::spawn(async move {
+            let query = Arc::clone(&shared_vector);
+            cpu_tasks.push(task::spawn_blocking(move || -> eyre::Result<(usize, Vec<u16>, u64)> {
                 let cpu_start = Instant::now();
-                let distances = task::spawn_blocking(move || {
-                    db[chunk_start..chunk_end]
-                        .iter()
-                        .map(|db_vec| udot(&query.0, db_vec))
-                        .collect::<Vec<_>>()
-                })
-                .await
-                .context("CPU worker task panicked")?;
+                let distances = db[chunk_start..chunk_end]
+                    .iter()
+                    .map(|db_vec| udot(&query.0, db_vec))
+                    .collect::<Vec<_>>();
                 let cpu_us = cpu_start.elapsed().as_micros() as u64;
+                Ok((chunk_id, distances, cpu_us))
+            }));
+        }
 
+        let mut network_tasks = FuturesUnordered::new();
+        let mut chunk_results = vec![None; chunk_count];
+        let mut cpu_times = Vec::new();
+        let mut network_times = Vec::new();
+        let mut galois_times = Vec::new();
+        let mut lte_times = Vec::new();
+
+        while let Some(cpu_res) = cpu_tasks.next().await {
+            let (chunk_id, distances, cpu_us) =
+                cpu_res.context("CPU worker join error")??;
+            cpu_times.push(cpu_us);
+
+            let mut session = self.network.sessions.get().await?;
+            network_tasks.push(task::spawn(async move {
                 let network_start = Instant::now();
                 let galois_start = Instant::now();
                 let mut chunk_distances = galois_ring_to_rep3(
-                    &mut *session,
+                    &mut session,
                     RingElement::convert_vec_rev(distances),
                 )
                 .await?;
@@ -343,43 +352,31 @@ impl Actor {
 
                 chunk_distances
                     .iter_mut()
-                    .for_each(|share| sub_pub(&mut *session, share, RingElement(THRESHOLD)));
+                    .for_each(|share| sub_pub(&mut session, share, RingElement(THRESHOLD)));
 
                 let lte_start = Instant::now();
                 let chunk_results =
-                    lte_threshold_and_open_u16(&mut *session, &chunk_distances).await?;
+                    lte_threshold_and_open_u16(&mut session, &chunk_distances).await?;
                 let lte_us = lte_start.elapsed().as_micros() as u64;
                 let network_us = network_start.elapsed().as_micros() as u64;
 
-                let _ = result_tx
-                    .send((chunk_id, chunk_results, cpu_us, network_us, galois_us, lte_us))
-                    .await;
-                eyre::Ok(())
-            });
-            worker_handles.push(handle);
+                eyre::Result::<_>::Ok((chunk_id, chunk_results, network_us, galois_us, lte_us))
+            }));
         }
-        drop(result_tx);
 
-        let mut chunk_results = vec![Vec::new(); chunk_count];
-        let mut cpu_times = Vec::new();
-        let mut network_times = Vec::new();
-        let mut galois_times = Vec::new();
-        let mut lte_times = Vec::new();
-        while let Some((chunk_id, results, cpu_us, network_us, galois_us, lte_us)) =
-            result_rx.recv().await
-        {
-            chunk_results[chunk_id] = results;
-            cpu_times.push(cpu_us);
+        while let Some(net_res) = network_tasks.next().await {
+            let (chunk_id, results, network_us, galois_us, lte_us) =
+                net_res.context("Network worker join error")??;
+            chunk_results[chunk_id] = Some(results);
             network_times.push(network_us);
             galois_times.push(galois_us);
             lte_times.push(lte_us);
         }
 
-        for handle in worker_handles {
-            handle.await??;
-        }
-
-        let results = chunk_results.into_iter().flatten().collect_vec();
+        let results = chunk_results
+            .into_iter()
+            .flat_map(|entry| entry.unwrap_or_default())
+            .collect_vec();
         let cpu_us = cpu_times.iter().max().copied().unwrap_or(0);
         let network_us = network_times.iter().max().copied().unwrap_or(0);
         let total_us = start_total.elapsed().as_micros() as u64;
